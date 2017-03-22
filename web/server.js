19f772d9dc2b8171bf47fadd46f2bc62d164172c
Sille Van Landschoot <info@sillevl.be>
2017-03-22 15:26:12 +0100

    working flip-counter component with bug

--- web/server.js
+++ web/server.js
@@ -14,6 +14,6 @@ app.get('/:sensor_id?', function (req, res) {
   res.render('home', { sensor_id: req.params.sensor_id || "demo"} )
 })
 
-app.listen(3000, function () {
+app.listen(3000, '0.0.0.0', function () {
   console.log('Example app listening on port 3000!')
 })
